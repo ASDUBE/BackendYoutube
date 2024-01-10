90d60063903b809d532b4b8745d8c217011744a0
ASDUBE <ashutoshd6198@gmail.com>
2024-01-10 16:07:27 +0530

    Connection

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { DB_Name } from "./constants"
+
+import connectDB from "./db/index.js";
+import dotenv from "dotenv"
+
+dotenv.config({ 
+  path: './env'
+})
+
+
+
+
+
+connectDB()

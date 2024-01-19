c8f8a163b79104c740d1570a8f83f2e5f610f937
ASDUBE <ashutoshd6198@gmail.com>
2024-01-20 01:12:27 +0530

    AccessToken

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -173,9 +173,62 @@ const logoutUser = asyncHandler (async(req , res) => {
 })
 
 
+const refreshAccessToken = asyncHandler(async (req, res) =>{
+  const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
+
+  if (!incomingRefreshToken){
+    throw new ApiError(401 , "unauthorized request")
+  }
+
+  try {
+    const decodedToken = jwt.verify(
+      incomingRefreshToken , process.env.REFRESH_TOKEN_SECRET
+    )
+  
+   
+  
+    const user = await User.findById(decodedToken?._id)
+  
+    if (!user){
+      throw new ApiError(401 , "Invalid Refresh Token")
+    }
+  
+  
+    if (incomingRefreshToken !== user?.refreshToken){
+      throw new ApiError (401, "Refresh Token Expired")
+    }
+  
+    const options ={
+      httpOnly: true,
+      secure: true,
+    }
+  
+    const {accessToken , newRefreshToken} = await generateAccessAndRefereshTokens(user._id)
+  
+    return res.status
+    .cookie("accessToken" , accessToken , options)
+    .cookie("refreshToken", newRefreshToken, options)
+    .json (
+      new ApiResponse(
+        200,
+        {
+          accessToken, refreshToken: newRefreshToken
+        },
+        "Access Token Refreshed"
+      )
+    )
+  } catch (error) {
+
+    throw new ApiError (401, error?.message || "Invalid Refresh Token")
+    
+  }
+
+})
+
 export {
     registerUser,
     loginUser,
     logoutUser,
+    refreshAccessToken,
     
 }
